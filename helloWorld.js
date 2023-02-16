2e6da7bf004ee923fe92a9a1c8856b3746ddcea5
Snehalkumbhar099 <snehalkumbhar099@gmail.com>
2023-02-16 18:52:10 +0530

    use of var  and console.log
    use of var  and console.log

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,35 @@
-console.log("mohit sharma");
+console.log("************* Step 1 ************");
+var myFullName;
+console.log(myFullName);
+myFullName = "Snehal Kumbhar";
+console.log("my full name is", myFullName);
+myFullName = "Snehal Vitthal Kumbhar";
+console.log("my full name is ", myFullName);
+console.log("************* Step 2 ************");
+var myLove ="I love only JS"
+console.log(myLove);
+console.log("************* Step 3 ************");
+
+var firstName = "snehal";
+var lastName = " kumbhar";
+var age = "25";
+var address = " vakad, pune";
+var pincode = "412115";
+var gender = "female";
+var married = "Yes";
+console.log(
+  "my name is:",
+  firstName,
+  " ,my surname is:",
+  lastName,
+  ", my age is:",
+  age,
+  " ,my address:",
+  address,
+  ",my pincode is:",
+  pincode,
+  ",my gender is:",
+  gender,
+  ",are you marrird:",
+  married
+);
