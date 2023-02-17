1191bb5131eb2372b955d0043479e58eaf7221f9
Snehalkumbhar099 <snehalkumbhar099@gmail.com>
2023-02-17 10:41:13 +0530

    completed 02_typeOf assignment
    completed 02_typeOf assignment

---
+++ 02_typeOf.js
@@ -0,0 +1,34 @@
+console.log("****************************   1    **************************");
+console.log("---------------Before Initialization-----------------");
+var bankName;
+console.log("Bank name is:", bankName);
+console.log("type of  Bankname is:", typeof  bankName);
+console.log("---------------After Initialization-----------------");
+bankName =" SBI";
+console.log("Bank name is:", bankName);
+console.log("type of  Bankname   is:", typeof  bankName);
+console.log("****************************   2    **************************");
+
+var mark = 90 ; 
+console.log("value of mark  variable is:", mark);
+console.log("type of  mark  is:", typeof  mark);
+
+console.log("****************************   3    **************************");
+console.log("---------------Before Initialization-----------------");
+var isWorking;
+console.log(" value of isworking is:", isWorking);
+console.log("type of  isWorking is:", typeof  isWorking);
+console.log("---------------After Initialization-----------------");
+isWorking = true;
+console.log("value of isworking is:", isWorking);
+console.log("type of  isWorking is:", typeof  isWorking);
+
+console.log("****************************   4   **************************");
+console.log("--------------- Declare and initialize-----------------");
+var totaleCount = " hundred and seven";
+console.log(" value of totaleCount is:", totaleCount);
+console.log("type of  totaleCount is:", typeof  totaleCount);
+console.log("--------------- update variable -----------------");
+var totaleCount = 107;
+console.log(" value of totaleCount is:", totaleCount);
+console.log("type of  totaleCount is:", typeof  totaleCount);
