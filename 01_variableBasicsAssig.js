4c67d3858da9b09c51a3579d0fe236aed3b81919
Snehalkumbhar099 <snehalkumbhar099@gmail.com>
2023-02-17 10:05:43 +0530

    Assignment 01 completed
    Assignment 01 completed

---
+++ 01_variableBasicsAssig.js
@@ -0,0 +1,47 @@
+var pincode = 412115;
+console.log("value of pincode variable is:",pincode);
+console.log("type of pincode is:", typeof pincode);
+
+var age = 25;
+console.log("value of age variable is:",age);
+console.log("type of age is:", typeof age);
+ 
+var  familyMembers =3;
+console.log("value of familyMembers variable is:",familyMembers);
+console.log("type of familyMembers is:", typeof familyMembers);
+ 
+ var firstName ='snehal';
+ console.log("value of firstName variable is:",firstName );
+console.log("type of firstName  is:", typeof firstName );
+
+var city ='pune';
+console.log("value of city  variable is:",city );
+console.log("type of city   is:", typeof city  );
+
+ var isGraduation = 'true';
+ console.log("value of  isGraduation variable is:",isGraduation);
+console.log("type of isGraduation   is:", typeof isGraduation);
+
+var isMarried = true;
+console.log("value of   isMarried  variable is:", isMarried);
+console.log("type of  isMarried    is:", typeof  isMarried);
+
+var isworking = false;
+console.log("value of   isworking  variable is:", isworking);
+console.log("type of  isworking   is:", typeof  isworking);
+
+var isGraduation = true;
+console.log("value of  isGraduation variable is:",isGraduation);
+console.log("type of isGraduation   is:", typeof isGraduation);
+
+ console.log("Difference between null and undefined : null means an empty value and its primitive data type and undefined means the variable has declared but its value is not assigned .");
+
+
+
+
+
+
+
+ 
+
+
