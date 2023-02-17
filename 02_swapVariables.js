8d35ba0f56aa62e4bdf1821a8fde73ca77f62d7b
Snehalkumbhar099 <snehalkumbhar099@gmail.com>
2023-02-17 11:14:54 +0530

    completed 02_swapVariable assignment
    completed 02_swapVariable assignment

---
+++ 02_swapVariables.js
@@ -0,0 +1,22 @@
+console.log("*******************   1     *********************");
+console.log("-------------Before swap------------");
+var sweety = "sweety";
+var cutie = " cutie"
+console.log( sweety,cutie);
+console.log("-------------After swap------------");
+temp=cutie;
+sweety=cutie;
+temp=sweety;
+console.log( sweety,temp);
+console.log("*******************   2     *********************");
+console.log("-------------Before swap------------");
+var num = 100;
+var num1 = 200;
+var num2 = 300;
+console.log(num,num1,num2);
+console.log("-------------After swap------------");
+temp=num1;
+num1=num;
+num=num2;
+num2=temp
+console.log(num,num1,num2);
