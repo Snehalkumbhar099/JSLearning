748322b40de732d03d89280c1a3407bf3de1052d
Snehalkumbhar099 <snehalkumbhar099@gmail.com>
2023-03-01 13:28:31 +0530

    for loop Assignment  01
    for loop Assignment  01 completed

---
+++ 06_for_loopAssig.js
@@ -0,0 +1,37 @@
+console.log(`**************   1   ****************`);
+
+for (let index = 5; index <=15; index++) {
+  console.log(index);
+    
+}
+console.log(`**************   2   ****************`);
+for (let index = 50; index >=40; index--) {
+    console.log(index);
+    
+}
+console.log(`**************   3   ****************`);
+for (let index = 3; index <=31; index=index+2) {
+    console.log(index);
+    
+}
+console.log(`**************   4   ****************`);
+for (let index = 2; index <=20; index=index+2) {
+    console.log(index);
+    
+}
+console.log(`**************   5   ****************`);
+for (let index = 5; index <=50; index=index+5) {
+    console.log(index);
+    
+}
+console.log(`**************   6   ****************`);
+for (let index = 10; index <=100; index=index+10) {
+    console.log(index);
+    
+}
+console.log(`**************   7   ****************`);
+
+for (let index = 100; index >=10; index=index-10) {
+    console.log(index);
+    
+}
